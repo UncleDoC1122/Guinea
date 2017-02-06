863afd5e2ab54f5a60c2280c8e0f844039b51b73
UncleDoC1122 <uncledoctortheone@gmail.com>
2017-02-07 00:37:18 +0300

    Fixed minor bugs

--- Guinea/Form1.cs
+++ Guinea/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,13 @@ namespace Guinea
         private void button1_Click(object sender, EventArgs e)
         {
             List<string> rawInfo = SiteDownloader.getter();
+
+            //File.Create("D:\\Test.txt");
+
+            for (int i = 0; i < rawInfo.Count; i++)
+            {
+                File.AppendAllText("D:\\Test.txt", rawInfo[i] + "/n");
+            }
 
             ExcelPutter.put(rawInfo);
         }
